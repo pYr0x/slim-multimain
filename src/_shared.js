bbb18ba54fb11703e4a7ce0561b9039fe67b7e09
Julian Kern <kern.julian@gmx.de>
2018-07-28 18:15:49 +0200

    working example

--- src/_shared.js
+++ src/_shared.js
@@ -1,11 +1,1 @@
-import {ajax} from "can";
-
 console.log("shared deps");
-
-ajax.ajaxSetup({
-  beforeSend: function (xhr){
-    if(jwt){
-      xhr.setRequestHeader("Authorization", "Bearer foobar");
-    }
-  }
-});
--- src/foo/main2.js
+++ src/foo/main2.js
@@ -1,5 +1,2 @@
 import "../_shared";
-import {DefineMap} from "can";
 console.log("main2");
-
-export const ViewModel = DefineMap.extend("VM", {});
